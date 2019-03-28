82cbb67da834445784f582ac544721420c6a9f05
Nhã Nguyễn Vạn <nha282@gmail.com>
2019-03-28 14:35:29 +0700

    update

--- model.py
+++ model.py
@@ -5,74 +5,19 @@ import numpy as np
 import pandas as pd
 import utils
 from keras.models import load_model
-def vgg_face(weights_path=None):
-    img = Input(shape=(224, 224,3))
-
-    pad1_1 = ZeroPadding2D(padding=(1, 1))(img)
-    conv1_1 = Conv2D(64, (3, 3), activation='relu', name='conv1_1')(pad1_1)
-    pad1_2 = ZeroPadding2D(padding=(1, 1))(conv1_1)
-    conv1_2 = Conv2D(64, (3, 3), activation='relu', name='conv1_2')(pad1_2)
-    pool1 = MaxPooling2D((2, 2), strides=(2, 2))(conv1_2)
-
-    pad2_1 = ZeroPadding2D((1, 1))(pool1)
-    conv2_1 = Conv2D(128, (3, 3), activation='relu', name='conv2_1')(pad2_1)
-    pad2_2 = ZeroPadding2D((1, 1))(conv2_1)
-    conv2_2 = Conv2D(128, (3, 3), activation='relu', name='conv2_2')(pad2_2)
-    pool2 = MaxPooling2D((2, 2), strides=(2, 2))(conv2_2)
-
-    pad3_1 = ZeroPadding2D((1, 1))(pool2)
-    conv3_1 = Conv2D(256, (3, 3), activation='relu', name='conv3_1')(pad3_1)
-    pad3_2 = ZeroPadding2D((1, 1))(conv3_1)
-    conv3_2 = Conv2D(256, (3, 3), activation='relu', name='conv3_2')(pad3_2)
-    pad3_3 = ZeroPadding2D((1, 1))(conv3_2)
-    conv3_3 = Conv2D(256, (3, 3), activation='relu', name='conv3_3')(pad3_3)
-    pool3 = MaxPooling2D((2, 2), strides=(2, 2))(conv3_3)
-
-    pad4_1 = ZeroPadding2D((1, 1))(pool3)
-    conv4_1 = Conv2D(512, (3, 3), activation='relu', name='conv4_1')(pad4_1)
-    pad4_2 = ZeroPadding2D((1, 1))(conv4_1)
-    conv4_2 = Conv2D(512, (3, 3), activation='relu', name='conv4_2')(pad4_2)
-    pad4_3 = ZeroPadding2D((1, 1))(conv4_2)
-    conv4_3 = Conv2D(512, (3, 3), activation='relu', name='conv4_3')(pad4_3)
-    pool4 = MaxPooling2D((2, 2), strides=(2, 2))(conv4_3)
-
-    pad5_1 = ZeroPadding2D((1, 1))(pool4)
-    conv5_1 = Conv2D(512, (3, 3), activation='relu', name='conv5_1')(pad5_1)
-    pad5_2 = ZeroPadding2D((1, 1))(conv5_1)
-    conv5_2 = Conv2D(512, (3, 3), activation='relu', name='conv5_2')(pad5_2)
-    pad5_3 = ZeroPadding2D((1, 1))(conv5_2)
-    conv5_3 = Conv2D(512, (3, 3), activation='relu', name='conv5_3')(pad5_3)
-    pool5 = MaxPooling2D((2, 2), strides=(2, 2))(conv5_3)
-
-    conv6_3 = Conv2D(4096,(7,7), activation='relu')(pool5)
-    conv6_3_drop = Dropout(0.5)(conv6_3)
-    fc7 = Conv2D(4096,(1,1) , activation='relu', name='fc7')(conv6_3_drop)
-    fc_flat = Flatten()(conv6_3_drop)
-    fc7_drop = Dropout(0.5)(fc7)
-    out = Conv2D(2622, (1, 1))(fc7_drop)
-    out = Flatten()(out)
-
-    model = Model(input=img, output=out)
-
-    if weights_path:
-        model.load_weights(weights_path)
-    
-    out2 = Dense(1000, activation='softmax', name='fc8')(fc_flat)
-    model2 = Model(input=img, output=out2)
-    return model2
 
 model = load_model('../local/data_face/keras-facenet/model/facenet_keras.h5')
 
-#model = vgg_face('../local/data_face/weights/vgg_face_weights.h5')
 data_train = pd.read_csv('../local/data_face/train.csv')
 img_names = data_train.image.tolist()
 img_arrays = utils.read_images(img_names)
-print(img_arrays.shape)
 labels = data_train.label.tolist()
 model.summary()
 model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 face_vectors = model.predict(img_arrays)
-print(face_vectors)
-model.fit(img_arrays,labels,batch_size=16,epochs=50,
-          validation_split=0.1)
-model.save('./model/model_cnn_face.h5')
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
+clf = svm.SVC(kernel='linear', C = 1.0)
+scores = cross_val_score(clf, face_vectors, labels, cv=5)
+print(scores)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))

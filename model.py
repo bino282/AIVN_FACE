2c54b27f07561c1ba6e6e70df74942da49d5605f
Nhã Nguyễn Vạn <nha282@gmail.com>
2019-03-28 15:05:23 +0700

    update

--- model.py
+++ model.py
@@ -5,7 +5,8 @@ import numpy as np
 import pandas as pd
 import utils
 from keras.models import load_model
-
+import os
+import numpy as p
 model = load_model('../local/data_face/keras-facenet/model/facenet_keras.h5')
 
 data_train = pd.read_csv('../local/data_face/train.csv')
@@ -13,11 +14,23 @@ img_names = data_train.image.tolist()
 img_arrays = utils.read_images(img_names)
 labels = data_train.label.tolist()
 model.summary()
-model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 face_vectors = model.predict(img_arrays)
 from sklearn import svm
 from sklearn.model_selection import cross_val_score
-clf = svm.SVC(kernel='linear', C = 1.0)
+clf = svm.SVC(kernel='linear', C = 1.0,probability=True)
 scores = cross_val_score(clf, face_vectors, labels, cv=5)
 print(scores)
 print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+test_dir = '../local/data_face/test'
+test_names = os.listdir(test_dir)
+test_imgs = utils.read_images(test_names)
+test_vector = model.predict(test_imgs)
+results = []
+for v in test_vector:
+    prob= clf.predict_proba(v)
+    ids = prob.argsort()[::-1][0:5]
+    results.append(ids)
+print(results)
+
+
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import numpy as np
 def read_images(names,mode='train'):
     imgs= []
     for name in names:
-        im = cv2.imread('../local/data_face/train/'+name)
+        im = cv2.imread('../local/data_face/{}/'.format(mode)+name)
         im = cv2.resize(im, (160, 160)) 
         imgs.append(im)
     return prewhiten(np.asarray(imgs))

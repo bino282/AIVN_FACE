04a221cb74ecc6e441983a835a4061fc60e5634b
Nhã Nguyễn Vạn <nha282@gmail.com>
2019-03-28 14:29:44 +0700

    update

--- utils.py
+++ utils.py
@@ -4,6 +4,6 @@ def read_images(names,mode='train'):
     imgs= []
     for name in names:
         im = cv2.imread('../local/data_face/train/'+name)
-        im = cv2.resize(im, (224, 224)) 
+        im = cv2.resize(im, (160, 160)) 
         imgs.append(im)
     return np.asarray(imgs)

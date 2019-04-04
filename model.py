cf94423ed11cb856d7492f00943af874f1a8a354
Nhã Nguyễn Vạn <nha282@gmail.com>
2019-04-04 11:02:06 +0700

    update

--- model.py
+++ model.py
@@ -7,12 +7,34 @@ from utils import *
 from keras.models import load_model
 import os
 import numpy as p
+import keras.layers as kl
 from keras import backend as K
 from sklearn.preprocessing import normalize
 def triplet_loss(y_true, y_pred):
     margin = K.constant(1)
     return K.mean(K.maximum(K.constant(0), K.square(y_pred[:,0,0]) - 0.5*(K.square(y_pred[:,1,0])+K.square(y_pred[:,2,0])) + margin))
-model = load_model('../local/data_face/keras-facenet/model/facenet_keras.h5')
+def euclidean_distance(vects):
+    x, y = vects
+    return K.sqrt(K.maximum(K.sum(K.square(x - y), axis=1, keepdims=True), K.epsilon()))
+model_facenet = load_model('../local/data_face/keras-facenet/model/facenet_keras.h5')
+
+def my_model(old_model):
+    input_anchor = old_model.inputs
+    input_positive = old_model.inputs
+    input_negative = old_model.inputs
+    anchor_rep = old_model(input_anchor)
+    pos_rep = old_model(input_positive)
+    neg_rep = old_model(input_negative)
+
+    positive_dist = kl.Lambda(euclidean_distance, name='pos_distance')([anchor_rep, pos_rep])
+    negative_dist = kl.Lambda(euclidean_distance, name='neg_distance')([anchor_rep, neg_rep])
+    tertiary_dist = kl.Lambda(euclidean_distance, name='ter_distance')([pos_rep, neg_rep])
+
+    stacked_dists = kl.Lambda(lambda vects: K.stack(vects, axis=1), name='stacked_dists')([positive_dist, negative_dist, tertiary_dist])
+
+    model_final = Model([input_anchor, input_positive, input_negative], stacked_dists, name='triple_siamese')
+
+    return model_final
 
 
 
@@ -22,9 +44,11 @@ img_arrays = read_images(img_names)
 labels = data_train.label.tolist()
 
 anchor,pos,neg = generate_triplets(img_arrays,np.asarray(labels),len(labels))
-print(model.summary())
-model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-model.fit(img_arrays,labels,epochs=500,validation_split=0.1,verbose=1)
+
+my_model = my_model(model_facenet)
+print(my_model.summary())
+my_model.compile(loss=triplet_loss,optimizer='adam',metrics=['accuracy'])
+my_model.fit([anchor,pos,neg],labels,epochs=500,validation_split=0.1,verbose=1)
 test_dir = '../local/data_face/test'
 test_names = os.listdir(test_dir)
 test_imgs = read_images(test_names,mode='test')
